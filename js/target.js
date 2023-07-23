8009fdefd4ba848607539f8d9ea9d762c38d7e61
Sunghoon Cho <sunghoch@c4r1s2.42seoul.kr>
2023-07-23 22:02:31 +0900

    regen target and click event

--- js/target.js
+++ js/target.js
@@ -1,22 +1,53 @@
-const gameArea = document.getElementById("game-area");
-const gameAreaRect = gameArea.getBoundingClientRect();
-const targetPositionX = (Math.floor(Math.random()) * gameAreaRect.width) + gameAreaRect.x;
-const targetPositionY = (Math.floor(Math.random()) * gameAreaRect.height) + gameAreaRect.y;
-const gameAreaConvas = document.querySelector("#game-area canvas");
+const gameCanvas = document.getElementById("game-canvas");
+const gameCanvasRect = gameCanvas.getBoundingClientRect();
+const gameScoreBoard = document.querySelector("#game-scoreboard");
+//gameAreaConvas
+let targetPositionX;
+let targetPositionY;
+let score = 0;
 
+function getRandom(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
 function newPosition() {
-	const targetPositionX = (Math.floor(Math.random()) * gameAreaRect.width) + gameAreaRect.x;
-	const targetPositionY = (Math.floor(Math.random()) * gameAreaRect.height) + gameAreaRect.y;
+	targetPositionX = getRandom(50, gameCanvas.width - 50);
+	targetPositionY = getRandom(50, gameCanvas.height - 50);
+	return [targetPositionX, targetPositionY];
 }
 function drawTarget() {
-	newPosition();
-	const ctx = gameAreaConvas.getContext("2d");
+	[targetPositionX, targetPositionY] = newPosition();
+	const ctx = gameCanvas.getContext("2d");
+	ctx.clearRect(0,0,gameCanvas.width, gameCanvas.height);
 	ctx.beginPath();
-	ctx.arc(targetPositionX, targetPositionY, 10, 0, 2 * Math.PI);
+	ctx.arc(targetPositionX, targetPositionY, 30, 0, 2 * Math.PI);
+	ctx.fillStyle = "green";
+	ctx.fill();
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.arc(targetPositionX, targetPositionY, 10, 0, 2*Math.PI);
 	ctx.fillStyle = "red";
 	ctx.fill();
 	ctx.stroke();
+	console.log(targetPositionX, targetPositionY);
 }
+function isInTarget(x, y) {
+	const dist = Math.sqrt((targetPositionX-x)*(targetPositionX-x) + (targetPositionY-y)*(targetPositionY-y))
+	console.log(dist);
+	if (dist <= 10) return 2;
+	if (dist <= 30) return 1;
+	else return 0;
+}
+function onClick(event) {
+	console.log(event.offsetX, event.offsetY);
+	const curScore = isInTarget(event.offsetX, event.offsetY);
+	if (curScore > 0) {
+		score += curScore;
+		gameScoreBoard.innerText = score;
+		drawTarget();
+	}
+}
+gameScoreBoard.innerText = score;
+drawTarget();
 
-drawTarget();
-drawTarget();
+gameCanvas.addEventListener("click", onClick);
